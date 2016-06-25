c269acfb382dc1d4ccf733b279c82c790b229e7d
ASealy <andrewjsealy@live.ca>
2016-06-25 00:46:57 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,133 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+ 
+ 
+  .state('app.about', {
+      url: '/about',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/about.html',
+       
+        }
+      }
+    })
+
+  
+   .state('app.chinese', {
+      url: '/chinese',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chinese.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+   .state('app.french', {
+      url: '/french',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/french.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+     .state('app.german', {
+      url: '/german',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/german.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+     .state('app.italian', {
+      url: '/italian',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/italian.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+     .state('app.japanese', {
+      url: '/japanese',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/japanese.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+     .state('app.russian', {
+      url: '/russian',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/russian.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+    
+    .state('app.spanish', {
+      url: '/spanish',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/spanish.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+ .state('app.turkish', {
+      url: '/turkish',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/turkish.html',
+          controller: 'Ctrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/about');
+});
